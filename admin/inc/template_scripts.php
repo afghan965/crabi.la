ebf8aae0710bfb99ff0c0c3bf94055c61c60699d
root <root@image.singlehop.net>
2014-02-27 06:02:08 -0600

    se solucionan urls del header

--- admin/inc/template_scripts.php
+++ admin/inc/template_scripts.php
@@ -14,6 +14,6 @@
 <script>!window.jQuery && document.write(unescape('%3Cscript src="js/vendor/jquery-1.11.0.min.js"%3E%3C/script%3E'));</script>
 
 <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
-<script src="js/vendor/bootstrap.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/app.js"></script>
+<script src="/js/vendor/bootstrap.min.js"></script>
+<script src="/js/plugins.js"></script>
+<script src="/js/app.js"></script>
