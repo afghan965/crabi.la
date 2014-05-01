302741ee55011ae00fef1ca5889b848bd7cab700
Pablo Quezada <pqzadaa@gmail.com>
2014-05-01 10:08:19 -0500

    modificaciones lalo

---
+++ carro/js/carro_notas.js
@@ -0,0 +1,52 @@
+	$(document).ready(function() {
+
+		$('.contenedor_cajas_seccion_nota').slick({
+		        dots: false,
+		        infinite: true,
+		        speed: 300,
+		        arrows: true,
+		        slidesToShow: 7,
+		        slidesToScroll: 1,
+		        
+		        responsive: [{
+		            breakpoint: 1024,
+		            settings: {
+		                slidesToShow: 7,
+		                slidesToScroll: 1,
+		                infinite: true,
+		                dots: true
+		            }
+		        }, {
+		            breakpoint: 641,
+		            settings: {
+				        dots: false,
+				        infinite: true,
+				        speed: 300,
+				        arrows: true,
+		                slidesToShow: 5,
+		                slidesToScroll: 1
+		            }
+		        }, {
+		            breakpoint: 480,
+		            settings: {
+				        dots: false,
+				        infinite: true,
+				        speed: 300,
+				        arrows: true,
+		                slidesToShow: 3,
+		                slidesToScroll: 1
+		            }
+		        }, {
+		            breakpoint: 360,
+		            settings: {
+				        dots: false,
+				        infinite: true,
+				        speed: 300,
+				        arrows: true,
+		                slidesToShow: 3,
+		                slidesToScroll: 1
+		            }
+		        }]
+		    });
+
+	});
